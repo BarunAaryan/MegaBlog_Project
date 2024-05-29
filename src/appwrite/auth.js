0b617fae1707159470432e43e26c1353874eb64c
Barun Aaryan <barunaryan1@gmail.com>
2024-05-29 13:16:42 +0530

    Built authentication service with appwrite

---
+++ src/appwrite/auth.js
@@ -0,0 +1,63 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+    //method
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                //call another method
+                return this.login({ email, password });
+
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            //check documentation - https://appwrite.io/docs/products/auth/email-password
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+    //check whether logged in or not
+    async getCurrentUser() {
+        try {
+            await this.account.get();
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error);
+        }
+        return null;
+    }
+    //logout - account.deletesession
+    async logout(){
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error", error);
+        }
+    }
+}
+
+const AuthService = new AuthService();
+export default authService
+
+
+
+
