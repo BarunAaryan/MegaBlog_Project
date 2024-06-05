58e04ffbd495abafa2f71bff30659bbb2e5b03ba
Barun Aaryan <barunaryan1@gmail.com>
2024-06-05 15:47:59 +0530

    Added few componentes

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -8,11 +8,19 @@ const Input = React.forwardRef(function Input(
   return (
     <div className="w-full">
       {/* if label pass hua hai then label component will be displayed*/}
-      {label && (
+      {label && 
         <label className="inline-block mb-1" htmlFor={id}>
           {label}
         </label>
-      )}
+      }
+      <input 
+      type={type}
+     className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+     ref={ref}
+     {...props}
+     id={id}
+
+      />
     </div>
   );
 });
